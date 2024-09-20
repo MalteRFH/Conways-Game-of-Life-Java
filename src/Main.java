29e2817b5e31dab89bb76fb5e6ba2c0f2d18efd3
MalteRFH <maltetorben.fuechsel@rfh-campus.de>
2024-09-20 12:24:10 +0200

    Array declaration and display method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
+    boolean[][] field;
+    int height = 10, width = 50;
+    public Main(){
+        field = new boolean[height][width];
+        for(boolean[] booleans : field){
+            Arrays.fill(booleans, false);
+        }
+    }
+
+    private void display(boolean[][] disp){
+        for (boolean[] booleans : disp) {
+            for (boolean aBoolean : booleans) {
+                if (aBoolean) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main();
     }
 }
