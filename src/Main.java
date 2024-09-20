cc786adc71df3978e8d5ac9b535e7fe971ef14df
MalteRFH <maltetorben.fuechsel@rfh-campus.de>
2024-09-20 13:47:50 +0200

    working Game with test model

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,76 @@
 import java.util.Arrays;
 
 public class Main {
-    boolean[][] field;
-    int height = 10, width = 50;
-    public Main(){
+    boolean[][] field, tempField;
+    int height = 40, width = 100;
+    public Main() throws InterruptedException {
         field = new boolean[height][width];
+        tempField = new boolean[height][width];
         for(boolean[] booleans : field){
             Arrays.fill(booleans, false);
         }
-    }
+        field[17][49] = true;
+        field[17][50] = true;
+        field[17][51] = true;
+        field[18][49] = true;
+        field[18][51] = true;
+        field[19][49] = true;
+        field[19][51] = true;
 
-    private void display(boolean[][] disp){
-        for (boolean[] booleans : disp) {
+        field[23][49] = true;
+        field[23][50] = true;
+        field[23][51] = true;
+        field[22][49] = true;
+        field[22][51] = true;
+        field[21][49] = true;
+        field[21][51] = true;
+        while(true){
+            display();
+            calcStates();
+            setField();
+            Thread.sleep(100);
+        }
+    }
+    private void calcStates(){
+        int aliveCount;
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                aliveCount = 0;
+                for(int k = -1; k < 2; k++){
+                    for(int l = -1; l < 2; l++) {
+                        if(i+k > -1 && j+l > -1 && i+k < height && j+l < width){
+                            if(field[i+k][j+l]) aliveCount++;
+                        }
+                    }
+                }
+                setTempField(aliveCount, i, j);
+            }
+        }
+    }
+    private void setTempField(int count, int y,int x){
+        if(field[y][x]) count--;
+        if(count == 2) tempField[y][x] = field[y][x];
+        else tempField[y][x] = count == 3;
+    }
+    private void setField(){
+        for(int i = 0; i < height; i++) {
+            if (width >= 0) System.arraycopy(tempField[i], 0, field[i], 0, width);
+        }
+    }
+    private void display(){
+        System.out.println("\n\n\n\n\n\n\n");
+        for (boolean[] booleans : field) {
             for (boolean aBoolean : booleans) {
                 if (aBoolean) {
-                    System.out.print("*");
+                    System.out.print("█");
                 } else {
-                    System.out.print("-");
+                    System.out.print("☐");
                 }
             }
             System.out.println();
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Main();
     }
 }
